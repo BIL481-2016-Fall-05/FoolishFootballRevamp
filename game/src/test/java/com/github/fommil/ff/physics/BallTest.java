bccb6b30105b4195a3037cdaf99728d95fe8395f
eduyayo <eduyayo@gmail.com>
2020-06-03 20:48:04 +0200

    test fix

--- game/src/test/java/com/github/fommil/ff/physics/BallTest.java
+++ game/src/test/java/com/github/fommil/ff/physics/BallTest.java
@@ -1,14 +1,14 @@
 /*
  * Copyright Samuel Halliday 2010
- * 
+ *
  * This file is free software: you can redistribute it and/or modify it under the terms of
  * the GNU General Public License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This file is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
  * PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this file.
  * If not, see <http://www.gnu.org/licenses/>.
  */
@@ -30,8 +30,8 @@ import com.github.fommil.ff.Pitch;
  * @author Samuel Halliday
  */
 public class BallTest {
-	
-	private static final double dt = 0.01;
+
+	private static final long DT = 100L;
 
 	private static final double EPSILON = 0.0001;
 
@@ -50,12 +50,12 @@ public class BallTest {
 		ball.setPosition(position);
 		ball.setVelocity(velocity);
 		for (int i = 0; i < 1000; i++) {
-			physics.step(dt);
+			physics.doTick(DT);
 			stepTest.test(ball.getPosition(), ball.getVelocity());
 		}
 		physics.clean();
 	}
-	
+
 	@Test
 	public void testNoKick() {
 		final Velocity velocity = new Velocity(0, 0, 0);
@@ -184,7 +184,7 @@ public class BallTest {
 		}
 	}
 
-	
+
 	@Test
 	public void testAfertouch() {
 		DummyPhysics physics = new DummyPhysics();
@@ -193,20 +193,20 @@ public class BallTest {
 		ball.setAftertouch(true);
 
 		ball.setVelocity(new Velocity(150,150,150));
-		
+
 		aftertouches.add(Aftertouch.UP);
 		aftertouches.add(Aftertouch.DOWN);
-	
+
 		aftertouches.add(Aftertouch.LEFT);
 		aftertouches.add(Aftertouch.LEFT);
-		
+
 
 		ball.setAftertouch(aftertouches);
 		DBody body =  ball.getGeom().getBody();
-				
+
 		assertEquals(body.getForce(), new DVector3(-2.4999999999999996, 2.4999999999999996, 6.0));
-		
+
 	}
 
-	
+
 }
--- game/src/test/java/com/github/fommil/ff/physics/GoalpostTest.java
+++ game/src/test/java/com/github/fommil/ff/physics/GoalpostTest.java
@@ -1,14 +1,14 @@
 /*
  * Copyright Samuel Halliday 2010
- * 
+ *
  * This file is free software: you can redistribute it and/or modify it under the terms of
  * the GNU General Public License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This file is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
  * PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this file.
  * If not, see <http://www.gnu.org/licenses/>.
  */
@@ -30,7 +30,7 @@ public class GoalpostTest {
 
 	private final Pitch pitch = new Pitch();
 
-	private static final double dt = 0.01;
+	private static final long DT = 100L;
 
 	interface Tester {
 
@@ -45,7 +45,7 @@ public class GoalpostTest {
 		ball.setPosition(position);
 		ball.setVelocity(velocity);
 		for (int i = 0; i < 1000; i++) {
-			physics.step(dt);
+			physics.doTick(DT);
 			stepTest.test(ball.getPosition(), ball.getVelocity());
 		}
 		physics.clean();
